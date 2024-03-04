1edc27004fe240d8c697ef886b87b708b6631b4c
ksyv <viais.kevin@hotmail.fr>
2024-03-04 11:58:22 +0100

    ad docstring for the modul, create class BaseModel and document them

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""This modul contain the base class for all models"""
 # this is the base class of all our models
+import uuid
+
+
+class BaseModel:
+    """Base class who contain:
+    Attributes:
+        - id
+        - created at
+        - updates_at
+    Methods:
+        - save()
+        - to_json()"""
+    
