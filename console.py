9dd99c5a0303897f29dd36265aaac374d24c5e2f
Hache_David <hache.david@free.fr>
2024-03-04 16:05:50 +0000

    Upgrade consol.py that contains the entry point of the command interpreter

--- console.py
+++ console.py
@@ -1,1 +1,19 @@
-#this is the entry point of our command interpreter
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    # Basic command
+    def do_quit(self, arg):
+        'Quit command to exit the program\n'
+        return True
+
+    def do_EOF(self, arg):
+        'EOF command to exit the program\n'
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
