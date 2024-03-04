26af154e48e0490250d4df26d57f2d92f0814c39
PhMLakeofGeneva <6897@holbertonstudents.com>
2024-03-04 15:55:38 +0000

    state.py

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains class City"""
+form models.base_model import BaseMode1
+
+
+class City(BaseModel):
+    """Class for City instances"""
+    state_id = ""
+    name = ""
