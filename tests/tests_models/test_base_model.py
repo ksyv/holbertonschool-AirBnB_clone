a9691b89fc6bf0136342f0d937a84375b3c226a2
ksyv <viais.kevin@hotmail.fr>
2024-03-04 15:00:00 +0100

    create test file

---
+++ tests/tests_models/test_base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""unitest for base_model"""
+import unittest
+
+
+class TestBaseModel(unittest.case):
+    """Test case for the BaseModel class"""
+    test = "j'en sais rien c'est pour essayer"
