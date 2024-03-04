d021877a76d3a26b41fb0d5583601fc35467c589
ksyv <viais.kevin@hotmail.fr>
2024-03-04 12:47:38 +0100

    ad def __str__

--- models/base_model.py
+++ models/base_model.py
@@ -18,4 +18,9 @@ class BaseModel:
         """init instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = 
+        
+    def __str__(self):
+        return (f"[<class name>] (<self.id>) <self.__dict__>")
+    
+    
