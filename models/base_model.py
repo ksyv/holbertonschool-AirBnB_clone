ff3cfbffdfdd425a84b4ee8a3b658a9b171d55dc
ksyv <viais.kevin@hotmail.fr>
2024-03-04 12:48:31 +0100

    fix missing expression delete by fault

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,8 @@ class BaseModel:
         """init instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = 
+        self.updated_at = datetime.now()
         
     def __str__(self):
         return (f"[<class name>] (<self.id>) <self.__dict__>")
-    
     
