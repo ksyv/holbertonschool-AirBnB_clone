6f96a793b8ba1099788bff2597ee063da1452ba0
ksyv <viais.kevin@hotmail.fr>
2024-03-05 09:20:02 +0100

    ad class FileStorage with private attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,10 @@
-#first file for storage classes
+#!/usr/bin/python3
+"""modul for file storage"""
+import json
+
+
+class FileStorage(self):
+    """serialize instances to a JSON file and deserialise JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+    
