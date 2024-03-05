c32c7d26fde96f5deaeb6297712283049e4452d2
ksyv <viais.kevin@hotmail.fr>
2024-03-05 14:50:13 +0100

    ad filestorage tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""unittest for file_storage"""
+import unittest
+from models.engine.file_storage import FileStorage
+import models
+import json
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test case for the FileStorage class"""
+    def test_Storage_initializes(self):
+        self.assertEqual(type(models.storage), FileStorage)
+    
+    def test_FileStorageWithoutArgs(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorageWithArg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_FileStorageFilePath(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_FileStorageObject(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
