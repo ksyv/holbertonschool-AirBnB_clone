3a61f2fbd4e87b7762f6dad0ec9729e1c6a6cb74
ksyv <viais.kevin@hotmail.fr>
2024-03-05 09:32:20 +0100

    ad def all(self) for return the dictionnary object and def new(self) for set in the dictionary the object with his key

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,17 @@
 import json
 
 
-class FileStorage(self):
+class FileStorage():
     """serialize instances to a JSON file and deserialise JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
-    
+
+    def all(self):
+        """return the dictionary object"""
+        return self.__object
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.name}.{obj.id}"
+        self.__obj[key] = obj
+
