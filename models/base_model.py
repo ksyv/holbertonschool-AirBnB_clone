a2aab336c965ac90b52aeee5f0b3c566e054cdb1
ksyv <viais.kevin@hotmail.fr>
2024-03-05 12:02:48 +0100

    change update_at attribution

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         if not kwargs or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             storage.new(self)
         else:
             for key, value in kwargs.items():
