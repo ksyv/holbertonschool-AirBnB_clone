c223791e7cdb671e66ead698c9194462817b8b03
ksyv <viais.kevin@hotmail.fr>
2024-03-05 10:07:32 +0100

    fix pycode style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,8 @@ import json
 
 
 class FileStorage():
-    """serialize instances to a JSON file and deserialise JSON file to instances"""
+    """serialize instances to a JSON file and deserialise
+    JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
@@ -20,7 +21,8 @@ class FileStorage():
     def save(self):
         """serializes __objects to the JSON file with path __file_path"""
         with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump({key: value.to_dict() for key, value in self.__objects.items()}, file)
+            json.dump({key: value.to_dict() for key, value in
+                       self.__objects.items()}, file)
 
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
