9cce63d6512e9a549c1c828eb69af121393e7bfd
ksyv <viais.kevin@hotmail.fr>
2024-03-05 09:55:50 +0100

    ad def save(self) and def reload(self) who serialize and deserialize

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,3 +17,17 @@ class FileStorage():
         key = f"{obj.__class__.name}.{obj.id}"
         self.__obj[key] = obj
 
+    def save(self):
+        """serializes __objects to the JSON file with path __file_path"""
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump({key: value.to_dict() for key, value in self.__objects.items()}, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects if __file_path exists"""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                instance = json.load(file)
+                for key, value in instance.items():
+                    self.__obj[key] = value
+        except FileNotFoundError:
+            pass
