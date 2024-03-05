a449effa304d3d51bb8063e5cb18215658ef5455
ksyv <viais.kevin@hotmail.fr>
2024-03-05 11:28:14 +0100

    first test

--- tests/tests_models/test_base_model.py
+++ tests/tests_models/test_base_model.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """unittest for base_model"""
 import unittest
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.Testcase):
     """Test case for the BaseModel class"""
-    test = "j'en sais rien c'est pour essayer"
+    def test_init_without_arguments(self):
+        """Test the __init__ method without arguments."""
+        self.assertEqual(BaseModel, type(BaseModel()))
 
 
 if __name__ == '__main__':
