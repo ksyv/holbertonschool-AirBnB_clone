f9e961e8384e3402e9101643ba953db65da440f6
ksyv <viais.kevin@hotmail.fr>
2024-03-05 12:06:30 +0100

    change method for to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -43,12 +43,8 @@ class BaseModel:
     def to_dict(self):
         """ returns a dictionary containing all keys/values
         of __dict__ of the instance, datetime in iso format"""
-        dictionary = {"__class__": self.__class__.__name__}
-        for key, value in self.__dict__.items():
-            if key == "created_at":
-                dictionary[key] = value.isoformat()
-            elif key == "updated_at":
-                dictionary[key] = value.isoformat()
-            else:
-                dictionary[key] = value
-        return dictionary
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
