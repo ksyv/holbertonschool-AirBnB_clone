1dff99916d76f65a1376b1e80d5f115a9eb8b4fb
ksyv <viais.kevin@hotmail.fr>
2024-03-06 14:47:39 +0100

    ad test for attributes: is string and is empty by default

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,6 +11,26 @@ class TestUserClass(unittest.TestCase):
         userTest = User()
         self.assertIsInstance(userTest, BaseModel)
 
+    def test_emailAttribute(self):
+        userTest = User()
+        self.assertEqual(userTest.email, "")
+        self.assertIsInstance(userTest.email, str)
+
+    def test_passwordAttribute(self):
+        userTest = User()
+        self.assertEqual(userTest.password, "")
+        self.assertIsInstance(userTest.password, str)
+
+    def test_first_nameAttribute(self):
+        userTest = User()
+        self.assertEqual(userTest.first_name, "")
+        self.assertIsInstance(userTest.first_name, str)
+
+    def test_last_nameAttribute(self):
+        userTest = User()
+        self.assertEqual(userTest.last_name, "")
+        self.assertIsInstance(userTest.last_name, str)
+
 
 if __name__ == '__main__':
     unittest.main()
