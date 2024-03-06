72dd3ce615cf03a0a94ec84a249364d3a73f5475
ksyv <viais.kevin@hotmail.fr>
2024-03-06 14:36:31 +0100

    add a test user for inheritance

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Modul for test the User class"""
+from models.base_model import BaseModel
+from models.user import User
+import unittest
+
+
+class TestUserClass(unittest.TestCase):
+    """Unittest for User class"""
+    def test_inheritance(self):
+        userTest = User()
+        self.assertIsInstance(userTest, BaseModel)
