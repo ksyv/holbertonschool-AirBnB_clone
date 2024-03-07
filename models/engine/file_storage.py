47da48fabaf8334b9e0867bcfbfa61ea5665c093
ksyv <viais.kevin@hotmail.fr>
2024-03-07 15:03:47 +0100

    add new classes infilestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,6 +27,12 @@ class FileStorage():
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.place import Place
         try:
             with open(self.__file_path, "r", encoding="utf-8") as file:
                 data = json.load(file)
