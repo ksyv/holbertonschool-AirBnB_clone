bb4e75cbdd4da4415562c010b89cd841ad7c0d24
ksyv <viais.kevin@hotmail.fr>
2024-03-07 13:30:22 +0100

    fix pycode style

--- models/place.py
+++ models/place.py
@@ -11,19 +11,18 @@ class Place(BaseModel1):
     Defines attributes/methods for the Place class, subclass of BasModel
     Other attributes/methods are inherited from BaseModel
     """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
 
-city_id = ""
-user_id = ""
-name = ""
-description = ""
-number_rooms = 0
-number_bathrooms = 0
-max_guest = 0
-price_by_night = 0
-latitude = 0.0
-longitude = 0.0
-amenity_ids = []
-
-# def __init__(self, *args, **kwargs):
-    """initialize variables and methods"""
-    super().__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initialize variables and methods"""
+        super().__(self, *args, **kwargs)
